14e6ef37131f51c113a74c939ecfb3651b9100a6
foolfighter <cristian.christensen@gmail.com>
2021-12-08 16:35:32 +0100

    Step 9: Add Bike & 'working?' method

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -1,4 +1,7 @@
 require 'bike.rb'
 describe Bike do
-
+  it 'responds to' do
+    bike = Bike.new
+    expect(bike).to respond_to(:working?)
+  end
 end  
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,5 +1,9 @@
 # spec/docking_station_spec.rb
 require 'docking_station.rb'
 describe DockingStation do
-it {expect(@release_bike).to be(Bike) }
+  it 'releases bike' do
+    docking_station = DockingStation.new
+    expect(docking_station.release_bike).to be(Bike) 
+  end
 end
+
