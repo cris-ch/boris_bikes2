bee675533952aba5335c7d197fb8610cdf22ba35
foolfighter <cristian.christensen@gmail.com>
2021-12-09 16:37:37 +0100

    Challenge 12: Using Instance Variables

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,6 +1,6 @@
 # spec/docking_station_spec.rb
 require 'docking_station.rb'
-# require 'bike.rb'
+require 'bike.rb'
 describe DockingStation do
   it 'releases bike' do
     docking_station = DockingStation.new
@@ -9,7 +9,7 @@ describe DockingStation do
 
   it 'checks if bike is working' do
     docking_station = DockingStation.new
-    bike = docking_station.release_bike
+    bike = Bike.new
 
     expect(bike.working?).to be(true)
   end
@@ -20,9 +20,17 @@ describe DockingStation do
   
   it 'docks bike' do
     docking_station = DockingStation.new
-    bike = docking_station.release_bike
+    bike = Bike.new
 
     expect(docking_station.dock(bike)).to be(bike)
+  end
+
+  it 'checks if there is a bike' do
+    docking_station = DockingStation.new
+    bike = Bike.new
+    docking_station.dock(bike)
+    
+    expect(docking_station.bike).to eq bike
   end
 
 end
