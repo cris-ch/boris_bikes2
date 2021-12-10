99968b071c06ec17a7e017c9d607ab154eaccbaa
foolfighter <cristian.christensen@gmail.com>
2021-12-10 12:08:50 +0100

    Challenge 14: Wrapping Collections

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -2,14 +2,19 @@ require_relative './bike.rb'
 class DockingStation
   attr_reader :bike
 
+  def initialize
+    @bikes_docked = []
+  end
+
   def release_bike
-    fail 'No bikes available' unless @bike
-    @bike
+    fail 'No bikes available' if @bikes_docked.empty?
+    @bikes_docked.pop
   end
 
   def dock(bike)
-    fail 'No space available' if @bike
-    @bike = bike
+    fail 'No space available' if @bikes_docked.size >= 20
+    @bikes_docked << bike
+    @bikes_docked[-1]
   end
 
   def bike
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,44 +2,24 @@
 require 'docking_station.rb'
 require 'bike.rb'
 describe DockingStation do
-
-  it 'checks if bike is working' do
-    docking_station = DockingStation.new
-    bike = Bike.new
-
-    expect(bike.working?).to be(true)
-  end
   
-  it 'check if there is a bike docked' do
-    is_expected.to respond_to(:bike)
+  it 'checks if there is a bike docked' do
+    expect(subject).to respond_to(:bike)
   end
   
   it 'docks bike' do
-    docking_station = DockingStation.new
     bike = Bike.new
-
-    expect(docking_station.dock(bike)).to be(bike)
+    expect(subject.dock(bike)).to be(bike)
   end
 
-  it 'check if a bike should be released' do
-    docking_station = DockingStation.new
-    bike = Bike.new
-    docking_station.dock(bike)
-
-    expect(docking_station.release_bike).to eq bike
-  end
-
-  it 'check a bike is not released when there are no bikes available' do
-    docking_station = DockingStation.new
-
-    expect {docking_station.release_bike}.to raise_error 'No bikes available'
+  it 'checks a bike is not released when there are no bikes available' do
+    expect {subject.release_bike}.to raise_error 'No bikes available'
   end
 
   it "doesn't dock a bike if no space available" do
-    docking_station = DockingStation.new
     bike = Bike.new
-    docking_station.dock(bike)
+    20.times { subject.dock(bike) }
 
-    expect {docking_station.dock(bike)}.to raise_error 'No space available'
+    expect {subject.dock(bike)}.to raise_error 'No space available'
   end
 end
